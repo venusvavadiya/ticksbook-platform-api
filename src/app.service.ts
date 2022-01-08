9204a7d17071d2bf438a3a9e50b9b7ccb81e294e
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2022-01-07 18:32:02 -0600

    chore: setup lint (#2)

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private readonly hello = 'Hello World!';
+
   getHello(): string {
-    return 'Hello World!';
+    return this.hello;
   }
 }
