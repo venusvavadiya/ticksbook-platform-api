0703937301c53a7cd2e14d49c400bebd2672cd31
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2022-01-15 14:19:40 -0600

    feat: read model entity

---
+++ src/platform-app/index.ts
@@ -0,0 +1,4 @@
+export * from './entity/order-book-entity';
+export * from './entity-repository/order-book-entity-repository';
+export * from './resolver/platform-mutation-resolver';
+export * from './resolver/platform-query-resolver';
